96198106f43b46ca3bdc5d63864964e0bbca2fc8
Luis Rivera-Wong <lriverawong@outlook.com>
2018-03-04 18:15:02 -0500

    created controllers for API

---
+++ guestbook/app/Http/Controllers/Api/ReportSignature.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReportSignature extends Controller
+{
+    /**
+     * Flag the given signature.
+     *
+     * When we retrieve the signature using Laravel Model Binding feature we call a flag
+     *  method on it which simply sets the flagged_at column value to the current datetime,
+     *  same way Laravel Soft Delete works. You can add this functionality by defining
+     *  this method in your Signature Model
+     * 
+     * @param Signature $signature
+     * @return Signature
+     */
+    public function update(Signature $signature)
+    {
+        $signature->flag();
+
+        return $signature;
+    }
+}
--- guestbook/app/Signature.php
+++ guestbook/app/Signature.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Signature extends Model
 {
-    // 
+    /**
+     * Ignore flagged signatures.
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIgnoreFlagged($query)
+    {
+        return $query->where('flagged_at', null);
+    }
+
+    /**
+     * Flag the given signature.
+     *
+     * @return bool
+     */
+    public function flag()
+    {
+        return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
+    }
 }
