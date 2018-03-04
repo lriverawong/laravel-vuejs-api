838d60bd774a677ed2a7f7117f93366e06f4a7d3
Luis Rivera-Wong <lriverawong@outlook.com>
2018-03-04 18:08:41 -0500

    created API routes

--- guestbook/routes/api.php
+++ guestbook/routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+Routing for API
+    GET: api/signatures this endpoint is responsible for fetching all signatures.
+    GET: api/signature/:id this endpoint is responsible for fetching a single signature by its ID.
+    POST: api/signatures this is the endpoint we will be hitting to save a new signature.
+    PUT: api/:id/report this endpoint is the one we will use to report a signature.
+
+*/
+Route::resource('signatures', 'Api\SignatureController')
+    ->only(['index', 'store', 'show']);
+Route::put('signatures/{signature}/report', 'Api\ReportSignature@update');
